270285520a01bb00e4f59c0e9c1229545ec4d312
Ohgyuchan <gyu3637@gmail.com>
2021-08-05 14:28:27 +0900

    Add firebase initialize App

--- lib/controllers/todoController.dart
+++ lib/controllers/todoController.dart
@@ -1,13 +1,12 @@
 import 'package:get/get.dart';
 import 'package:todo_app/controllers/authController.dart';
-import 'package:todo_app/controllers/bindings/authBinding.dart';
 import 'package:todo_app/models/todo.dart';
 import 'package:todo_app/services/database.dart';
 
 class TodoController extends GetxController {
-  Rx<List<TodoModel>> todoList = Rx<List<TodoModel>>();
+  var todoList = <TodoModel>[].obs;
 
-  List<TodoModel> get todos => todoList.value;
+  List<TodoModel> get todos => todoList;
 
   @override
   void onInit() {
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo_app/controllers/bindings/authBinding.dart';
 import 'package:todo_app/utils/root.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
